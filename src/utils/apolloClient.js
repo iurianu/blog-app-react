cf6fb61b33ca23b622cccc744972423c3bc8b1ad
Iulian Andriescu <iulianandriescu@gmail.com>
2022-05-20 21:34:19 +0300

    re-build

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -5,7 +5,7 @@ import { HttpLink } from "apollo-link-http"
 const cache = new InMemoryCache()
 
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
+  uri: `${process.env.STRAPI_URL}/graphql`
 })
 
 const client = new ApolloClient({
